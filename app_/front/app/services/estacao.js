1ca76b96441ebad1ac9e603f9cbc92f9d5300430
VictorLeite <victor.leite@gmail.com>
2016-10-17 16:50:31 -0200

    removendo pasta app_

--- app_/front/app/services/estacao.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.service('estacaoService',['$http','$log','utilService',function($http, $log, utilService) {
-	this.getEstacaoList = function() {		
-		return $http.get(utilService.getUrlServidor() + '/index.php?r=frontend/front/lista-de-estacoes');
-	};			
-
-} ]);
